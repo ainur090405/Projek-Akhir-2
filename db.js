a4a8bef5ef0ba4e67fa53bd645f8659ccb722f57
ainur090405 <ainurtaufikurrahman090405@gmail.com>
2025-05-17 08:27:59 +0700

    data awal

---
+++ db.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'posyandu'
+});
+db.connect(err => {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database.');
+});
+
+module.exports = db;
